06bacab75acce82a9cc2888ac2c7de0c80c1e0a0
Vlad Filiucov <filiucov@mail.ru>
2020-06-17 11:15:41 +0100

    add gendiff description options and arguments

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ const { Command } = commander;
 
 const program = new Command();
 
-program.helpOption('-h, --help', 'output usage information');
 program.version('0.0.1', '-v, --version', 'output the current version');
+program.description('Compares two configuration files and shows the difference.');
+program.helpOption('-h, --help', 'output usage information');
+program.option('-f, --format [type]', 'output format');
+program.arguments('<filepath1> <filepath2>').action((filepath1, filepath2) => {
+  console.log('FP1', filepath1);
+  console.log('FP2', filepath2);
+});
 
 export default program;
