4ae665f539bb2a566660b2e64ae5781209982c30
Vlad Filiucov <filiucov@mail.ru>
2020-06-18 23:25:27 +0100

    decouple script from module

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
 
-import program from '../src/index.js';
+import commander from 'commander';
+import gendiff from '../src/index.js';
+
+const { Command } = commander;
+
+const program = new Command();
+
+program.version('0.0.1', '-v, --version', 'output the current version');
+program.description('Compares two configuration files and shows the difference.');
+program.helpOption('-h, --help', 'output usage information');
+program.option('-f, --format [type]', 'output format');
+program.arguments('<filepath1> <filepath2>').action((filepath1, filepath2) => {
+  gendiff(filepath1, filepath2);
+});
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-import commander from 'commander';
-
-const { Command } = commander;
-
-const program = new Command();
-
-program.version('0.0.1', '-v, --version', 'output the current version');
-program.description('Compares two configuration files and shows the difference.');
-program.helpOption('-h, --help', 'output usage information');
-program.option('-f, --format [type]', 'output format');
-program.arguments('<filepath1> <filepath2>').action((filepath1, filepath2) => {
-  console.log('FP1', filepath1);
-  console.log('FP2', filepath2);
-});
-
-export default program;
+export default (fp1, fp2) => {
+  console.log('fp1', fp1);
+  console.log('fp2', fp2);
+};
