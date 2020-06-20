06b0036aef82e04e7b5f166ec23c054f5ae39bf8
Vlad Filiucov <filiucov@mail.ru>
2020-06-21 00:55:08 +0100

    add spec for gendiff function

--- src/__tests__/gendiff.js
+++ src/__tests__/gendiff.js
@@ -1,5 +1,35 @@
 import gendiff from '../index.js';
 
-it('works', () => {
-  expect(true).toBe(true);
+let beforeConfig;
+let afterConfig;
+
+beforeEach(() => {
+  beforeConfig = {
+    name: 'gendiff',
+    type: 'module',
+    version: '1.0.0',
+    description: 'CLI tool for comparing config files',
+  };
+
+  afterConfig = {
+    name: 'gendiff',
+    type: true,
+    version: '1.0.0',
+    description: 'CLI foo tool for comparing config files',
+  };
+});
+
+describe('gendiff', () => {
+  it('can generate diff for two objects', () => {
+    expect(gendiff(beforeConfig, afterConfig)).toBe(
+      `{
+    name: gendiff
+  - type: module
+  + type: true
+    version: 1.0.0
+  - description: CLI tool for comparing config files
+  + description: CLI foo tool for comparing config files
+}`,
+    );
+  });
 });
