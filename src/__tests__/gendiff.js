13defe777adb665f31c11fa23b44d1405ad05ed8
Vlad Filiucov <filiucov@mail.ru>
2020-06-24 10:38:34 +0100

    refactor test to use fixtures

--- src/__tests__/gendiff.js
+++ src/__tests__/gendiff.js
@@ -1,29 +1,12 @@
+import path from 'path';
 import gendiff from '../index.js';
-
-let beforeConfig;
-let afterConfig;
-
-beforeEach(() => {
-  beforeConfig = {
-    name: 'gendiff',
-    type: 'module',
-    version: '1.0.0',
-    description: 'CLI tool for comparing config files',
-    proxy: false,
-  };
-
-  afterConfig = {
-    name: 'gendiff',
-    type: true,
-    version: '1.0.0',
-    description: 'CLI foo tool for comparing config files',
-    verbose: true,
-  };
-});
 
 describe('gendiff', () => {
   it('can generate diff for two objects', () => {
-    expect(gendiff(beforeConfig, afterConfig)).toBe(
+    const beforeConfPath = path.join(process.cwd(), '__fixtures__', 'confBefore.json');
+    const afterConfPath = path.join(process.cwd(), '__fixtures__', 'confAfter.json');
+
+    expect(gendiff(beforeConfPath, afterConfPath)).toBe(
       `{
     name: gendiff
   - type: module
