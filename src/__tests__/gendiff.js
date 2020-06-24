efaa18796a34349cd3c0ee5a39cc3984c5831f7a
Vlad Filiucov <filiucov@mail.ru>
2020-06-24 11:46:23 +0100

    add spec for YAML format

--- src/__tests__/gendiff.js
+++ src/__tests__/gendiff.js
@@ -1,13 +1,10 @@
 import path from 'path';
 import gendiff from '../index.js';
 
-describe('gendiff', () => {
-  it('can generate diff for two objects', () => {
-    const beforeConfPath = path.join(process.cwd(), '__fixtures__', 'confBefore.json');
-    const afterConfPath = path.join(process.cwd(), '__fixtures__', 'confAfter.json');
+let formattedDiff;
 
-    expect(gendiff(beforeConfPath, afterConfPath)).toBe(
-      `{
+beforeAll(() => {
+  formattedDiff = `{
     name: gendiff
   - type: module
   + type: true
@@ -16,7 +13,25 @@ describe('gendiff', () => {
   + description: CLI foo tool for comparing config files
   - proxy: false
   + verbose: true
-}`,
-    );
+}`;
+});
+
+describe('gendiff', () => {
+  describe('in JSON format', () => {
+    it('can generate diff for two objects', () => {
+      const beforeConfPath = path.join(process.cwd(), '__fixtures__', 'confBefore.json');
+      const afterConfPath = path.join(process.cwd(), '__fixtures__', 'confAfter.json');
+
+      expect(gendiff(beforeConfPath, afterConfPath)).toBe(formattedDiff);
+    });
+  });
+
+  describe('in YAML format', () => {
+    it('can generate diff for two objects', () => {
+      const beforeConfPath = path.join(process.cwd(), '__fixtures__', 'confBefore.yml');
+      const afterConfPath = path.join(process.cwd(), '__fixtures__', 'confAfter.yaml');
+
+      expect(gendiff(beforeConfPath, afterConfPath)).toBe(formattedDiff);
+    });
   });
 });
