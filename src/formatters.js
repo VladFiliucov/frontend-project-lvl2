48d82932801d28a1b44d852ed72e9baec04479cb
Vlad Filiucov <filiucov@mail.ru>
2020-07-29 12:25:36 +0100

    add some offset to nested objects

--- src/formatters.js
+++ src/formatters.js
@@ -3,25 +3,28 @@ const BASE_OFFSET = 4;
 const modifications = {
   add: '+ ',
   remove: '- ',
-  keep: '',
+  keep: '  ',
 };
 
 const stylish = diffEntries => {
   const formatOutput = (entries, nestingLevel = 0, nestedKeyName, nestedKeyModification) => {
     const formatter = {
-      add: (key, value, offset = 2) => `${' '.repeat(2 * offset + (nestedKeyModification ? 2 : 0))}+ ${key}: ${value}`,
-      remove: (key, value, offset = 2) => `${' '.repeat(2 * offset)}- ${key}: ${value}`,
+      add: (key, value, offset = 2) =>
+        `${' '.repeat(2 * offset + (nestedKeyModification ? 2 : 0))}+ ${key}: ${value}`,
+      remove: (key, value, offset = 2) =>
+        `${' '.repeat(2 * offset + (nestedKeyModification ? 2 : 0))}- ${key}: ${value}`,
       keep: (key, value, offset = 4) => `${' '.repeat(4 * offset)}${key}: ${value}`,
     };
 
-    const keyWithModification = nestedKeyModification && `${modifications[nestedKeyModification].concat(nestedKeyName)}`;
+    const keyWithModification =
+      nestedKeyModification && `${modifications[nestedKeyModification].concat(nestedKeyName)}`;
+    console.log(nestedKeyName, nestingLevel);
 
     const start = nestedKeyName
-      ? `${' '.repeat(nestingLevel).concat(keyWithModification)}: {`
+      ? `${' '.repeat(nestingLevel - 2).concat(keyWithModification)}: {`
       : `${' '.repeat(nestingLevel)}{`;
     const end = `${' '.repeat(nestingLevel)}}`;
     const indentedEntries = entries.map(entry => {
-      // console.log(entry.keyName, entry.depth);
       const entryContent = Array.isArray(entry.children)
         ? formatOutput(entry.children, entry.depth * BASE_OFFSET, entry.keyName, entry.modification)
         : formatter[entry.modification](entry.keyName, entry.data, entry.depth);
@@ -34,10 +37,6 @@ const stylish = diffEntries => {
     return multilineDiff;
   };
 
-  const acc = [diffEntries[0], diffEntries[1], diffEntries[2], diffEntries[3], diffEntries[4]];
-
-  // console.log(formatOutput(diffEntries));
-  // return formatOutput(acc);
   return formatOutput(diffEntries);
 };
 
