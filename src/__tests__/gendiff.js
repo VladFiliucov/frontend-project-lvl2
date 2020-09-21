d67d95a26503d237889c38453f3e84ab441754a8
Vlad Filiucov <filiucov@mail.ru>
2020-09-21 23:48:12 +0100

    WIP start prototyping diff

--- src/__tests__/gendiff.js
+++ src/__tests__/gendiff.js
@@ -51,7 +51,7 @@ Property 'type' was updated. From 'module' to true
 Property 'setting6.ops' was added with value: 'vops'
 Property 'description' was updated. From 'CLI tool for comparing config files' to 'CLI foo tool for comparing config files'
 Property 'proxy' was removed
-Property 'next' was removed
+Property 'nest' was removed
 Property 'simple' was updated. From true to [complex value]
 Property 'subset.key.foo' was updated. From 'bar' to 'baz'
 Property 'verbose' was added with value: true
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,8 +1,28 @@
+const getChangelog = (entry, next) => {
+  if (entry.modification === 'keep') return;
+
+  if (typeof next === 'undefined') return 'last one';
+
+  if (entry.keyName === next.keyName) {
+    return 'updated from something to something';
+  } else if (entry.modification === 'add') {
+    return 'added';
+  } else {
+    return 'removed';
+  }
+};
+
 const plain = diffEntries => {
   const formattedEntries = [];
 
   diffEntries.forEach((entry, index) => {
-    formattedEntries.push(`Property ${entry.keyName} with index ${index}`);
+    const prev = diffEntries[index - 1];
+    // TODO think about next line logic
+    if (prev && entry.keyName === prev.keyName) return;
+
+    const changelog = getChangelog(entry, diffEntries[index + 1]);
+
+    if (changelog) formattedEntries.push(`Property '${entry.keyName}' was ${changelog}`);
   });
 
   const multilineDiff = formattedEntries.join('\n');
