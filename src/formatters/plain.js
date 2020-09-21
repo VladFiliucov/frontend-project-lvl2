5db851d1fab8213f71cfd5b705565ef6e0bf26cd
Vlad Filiucov <filiucov@mail.ru>
2020-09-21 23:14:27 +0100

    iterate over entries

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,1 +1,13 @@
-export default () => {};
+const plain = diffEntries => {
+  const formattedEntries = [];
+
+  diffEntries.forEach((entry, index) => {
+    formattedEntries.push(`Property ${entry.keyName} with index ${index}`);
+  });
+
+  const multilineDiff = formattedEntries.join('\n');
+
+  return multilineDiff;
+};
+
+export default plain;
