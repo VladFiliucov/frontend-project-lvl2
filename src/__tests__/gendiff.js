391a95cd615adf2e206308f50f4c2ce57392b0a5
Vlad Filiucov <filiucov@mail.ru>
2020-09-24 19:42:29 +0100

    remove extra newlines from spec file

--- src/__tests__/gendiff.js
+++ src/__tests__/gendiff.js
@@ -46,8 +46,7 @@ beforeAll(() => {
     }
 }`;
 
-  formattedPlainDiff = `
-Property 'type' was updated. From 'module' to true
+  formattedPlainDiff = `Property 'type' was updated. From 'module' to true
 Property 'setting6.ops' was added with value: 'vops'
 Property 'description' was updated. From 'CLI tool for comparing config files' to 'CLI foo tool for comparing config files'
 Property 'proxy' was removed
@@ -55,8 +54,7 @@ Property 'nest' was removed
 Property 'simple' was updated. From true to [complex value]
 Property 'subset.key.foo' was updated. From 'bar' to 'baz'
 Property 'verbose' was added with value: true
-Property 'new_object' was added with value: [complex value]
-  `;
+Property 'new_object' was added with value: [complex value]`;
 });
 
 describe('gendiff', () => {
