aaa5cee89d5fe116c67a66becb0aed2b66864800
Vlad Filiucov <filiucov@mail.ru>
2020-12-04 19:13:46 +0000

    refactor stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,85 +3,17 @@ import _ from 'lodash';
 const BASE_INDENTATION = 4;
 const SPACE_FOR_OPERATORS = 2;
 
-const modifications = {
-  addition: '+ ',
-  removal: '- ',
-  persisted: '  ',
-  parent: '  ',
-};
-
-const toString = (data, nestingLevel) => {
-  const currentIndentation = ' '.repeat(BASE_INDENTATION * (nestingLevel + 1));
-
-  if (_.isPlainObject(data)) {
-    const end = `${' '.repeat(BASE_INDENTATION * nestingLevel)}}`;
-    const entries = Object.keys(data).map(key => {
-      if (_.isPlainObject(data[key])) {
-        return `${currentIndentation}${key}: ${toString(data[key], nestingLevel + 1)}`;
-      }
-
-      return `${currentIndentation}${key}: ${data[key]}`;
-    });
-
-    return ['{', ...entries, end].join('\n');
-  }
-
-  return data;
-};
-
-const stylishx = diffEntries => {
-  const formatOutput = (entries, nestingDepth, nestedKeyName, nestedKeyModification) => {
-    const formatter = {
-      addition: ({ key, data, nestingLevel }) =>
-        `${' '.repeat(BASE_INDENTATION * nestingLevel - SPACE_FOR_OPERATORS)}+ ${key}: ${toString(
-          data,
-          nestingLevel,
-        )}`,
-      removal: ({ key, data, nestingLevel }) =>
-        `${' '.repeat(BASE_INDENTATION * nestingLevel - SPACE_FOR_OPERATORS)}- ${key}: ${toString(
-          data,
-          nestingLevel,
-        )}`,
-      persisted: ({ key, data, nestingLevel }) =>
-        `${' '.repeat(BASE_INDENTATION * nestingLevel)}${key}: ${data}`,
-      modified: ({ key, removedData, addedData, nestingLevel }) =>
-        `${' '.repeat(BASE_INDENTATION * nestingLevel - SPACE_FOR_OPERATORS)}- ${key}: ${toString(
-          removedData,
-          nestingLevel,
-        )}\n${' '.repeat(
-          BASE_INDENTATION * nestingLevel - SPACE_FOR_OPERATORS,
-        )}+ ${key}: ${toString(addedData, nestingLevel)}`,
-    };
-
-    const keyWithModification =
-      nestedKeyModification && `${modifications[nestedKeyModification].concat(nestedKeyName)}`;
-
-    const start = nestedKeyName
-      ? `${' '.repeat(nestingDepth - SPACE_FOR_OPERATORS).concat(keyWithModification)}: {`
-      : `${' '.repeat(nestingDepth)}{`;
-    const end = `${' '.repeat(nestingDepth)}}`;
-    const indentedEntries = entries.map(entry => {
-      const entryContent = Array.isArray(entry.children)
-        ? formatOutput(entry.children, entry.nestingLevel * BASE_INDENTATION, entry.key, entry.type)
-        : formatter[entry.type](entry);
-
-      return entryContent;
-    });
-    const result = [start, ...indentedEntries, end];
-    const multilineDiff = result.join('\n');
-
-    return multilineDiff;
-  };
-
-  return formatOutput(diffEntries);
-};
-
 const indent = depth => ' '.repeat(BASE_INDENTATION * depth - SPACE_FOR_OPERATORS);
 
 const dataFormatter = (data, depth) => {
   if (_.isPlainObject(data)) {
-    // Should we handle objects here?
-    return ['{', `${indent(depth)}  }`].join('\n');
+    const formattedObjectDiff = Object.entries(data)
+      .map(
+        ([entryKey, entryValue]) =>
+          `${indent(depth)}      ${entryKey}: ${dataFormatter(entryValue, depth + 1)}`,
+      )
+      .join('\n');
+    return `{\n${formattedObjectDiff}\n${indent(depth)}  }`;
   }
 
   return data;
@@ -103,7 +35,6 @@ const getChangelog = (node, depth) => {
   if (node.type === 'removal') {
     return `${indent(depth)}- ${node.key}: ${dataFormatter(node.data, depth)}`;
   }
-
   return null; // To make linter happy with consistent returns
 };
 
