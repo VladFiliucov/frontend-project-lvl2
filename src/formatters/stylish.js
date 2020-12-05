50c8e67ee4b5a241136e3122a143c58f130babfd
Vlad Filiucov <filiucov@mail.ru>
2020-12-05 13:10:43 +0000

    remove unnecessary return

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,19 +5,6 @@ const SPACE_FOR_OPERATORS = 2;
 
 const indent = depth => ' '.repeat(BASE_INDENTATION * depth - SPACE_FOR_OPERATORS);
 
-const dataFormatter = (data, depth) => {
-  if (_.isPlainObject(data)) {
-    const formattedObjectDiff = Object.entries(data)
-      .map(
-        ([entryKey, entryValue]) =>
-          `${indent(depth)}      ${entryKey}: ${dataFormatter(entryValue, depth + 1)}`,
-      )
-      .join('\n');
-    return [`{\n${formattedObjectDiff}\n${indent(depth)}  }`];
-  }
-  return data;
-};
-
 const getChangelog = (node, depth) => {
   if (node.type === 'persisted')
     return `${indent(depth)}  ${node.key}: ${dataFormatter(node.data, depth)}`;
@@ -25,13 +12,10 @@ const getChangelog = (node, depth) => {
     return `${indent(depth)}+ ${node.key}: ${dataFormatter(node.data, depth)}`;
   if (node.type === 'removal')
     return `${indent(depth)}- ${node.key}: ${dataFormatter(node.data, depth)}`;
-  if (node.type === 'modified') {
-    const removed = `${indent(depth)}- ${node.key}: ${dataFormatter(node.removedData, depth)}`;
-    const added = `${indent(depth)}+ ${node.key}: ${dataFormatter(node.addedData, depth)}`;
-    return [removed, added];
-  }
 
-  return null; // To make linter happy with consistent returns
+  const removed = `${indent(depth)}- ${node.key}: ${dataFormatter(node.removedData, depth)}`;
+  const added = `${indent(depth)}+ ${node.key}: ${dataFormatter(node.addedData, depth)}`;
+  return [removed, added];
 };
 
 const stylish = diffEntries => {
@@ -51,5 +35,19 @@ const stylish = diffEntries => {
 
   return ['{', ...diff, '}'].join('\n');
 };
+
+const dataFormatter = (data, depth) => {
+  if (_.isPlainObject(data)) {
+    const formattedObjectDiff = Object.entries(data)
+      .map(
+        ([entryKey, entryValue]) =>
+          `${indent(depth)}      ${entryKey}: ${dataFormatter(entryValue, depth + 1)}`,
+      )
+      .join('\n');
+    return [`{\n${formattedObjectDiff}\n${indent(depth)}  }`];
+  }
+  return data;
+};
+
 
 export default stylish;
