e0972710671e5a2ba8360c582ddb7ce40b850629
Vlad Filiucov <filiucov@mail.ru>
2020-12-08 18:25:48 +0000

    change formatted difs to be plain files instead of esm modules

--- __tests__/gendiff.js
+++ __tests__/gendiff.js
@@ -1,21 +1,25 @@
 import path from 'path';
+import { readFileSync } from 'fs';
+import _ from 'lodash';
 import gendiff from '../src/index.js';
-import formattedStylishDiff from './__fixtures__/formattedStylishDiff.js';
-import formattedPlainDiff from './__fixtures__/formattedPlainDiff.js';
-import formattedJSONDiff from './__fixtures__/formattedJSONDiff.js';
 
 const FIXTURES_PATH = ['__tests__', '__fixtures__'];
 
-const getFixture = (filename, options = { pathToFixtures: FIXTURES_PATH }) =>
+const getFixturePath = (filename, options = { pathToFixtures: FIXTURES_PATH }) =>
   path.join(process.cwd(), ...options.pathToFixtures, filename);
+
+const getFixtureContent = (filename, options = { pathToFixtures: FIXTURES_PATH }) =>
+  readFileSync(path.resolve([...options.pathToFixtures, filename].join('/')), 'utf8');
 
 describe('gendiff', () => {
   test.each(['json', 'yml'])('can generate dif in %s format', extension => {
-    const beforeConfPath = getFixture(`confBefore.${extension}`);
-    const afterConfPath = getFixture(`confAfter.${extension}`);
+    const beforeConfPath = getFixturePath(`confBefore.${extension}`);
+    const afterConfPath = getFixturePath(`confAfter.${extension}`);
+    const formattedStylishDiff = _.trim(getFixtureContent('formattedStylishDiff'));
+    const formattedPlainDiff = _.trim(getFixtureContent('formattedPlainDiff'));
+    const formattedJSONDiff = _.trim(getFixtureContent('formattedJSONDiff'));
 
     expect(gendiff(beforeConfPath, afterConfPath)).toBe(formattedStylishDiff);
-    expect(gendiff(beforeConfPath, afterConfPath, 'stylish')).toBe(formattedStylishDiff);
     expect(gendiff(beforeConfPath, afterConfPath, 'plain')).toBe(formattedPlainDiff);
     expect(gendiff(beforeConfPath, afterConfPath, 'json')).toBe(formattedJSONDiff);
   });
