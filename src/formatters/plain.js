7871361f46e8ea5de2a88ab3d0565d1dae44076b
Vlad Filiucov <filiucov@mail.ru>
2020-12-10 18:17:16 +0000

    use switch instead of bunch of if statements

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,17 +10,22 @@ const dataFormatter = data => {
 const getChangelog = (current, nameAcc = []) => {
   const keyName = [...nameAcc, current.key].join('.');
 
-  if (current.type === 'deleted') return `Property '${keyName}' was removed`;
-  if (current.type === 'added')
-    return `Property '${keyName}' was added with value: ${dataFormatter(current.data)}`;
-  if (current.type === 'modified')
-    return `Property '${keyName}' was updated. From ${dataFormatter(
-      current.removedData,
-    )} to ${dataFormatter(current.addedData)}`;
-  if (current.type === 'parent')
-    return _.compact(current.children.map(child => getChangelog(child, [...nameAcc, current.key])));
-
-  return null; // To make linter happy with consistent returns
+  switch (current.type) {
+    case 'deleted':
+      return `Property '${keyName}' was removed`;
+    case 'added':
+      return `Property '${keyName}' was added with value: ${dataFormatter(current.data)}`;
+    case 'modified':
+      return `Property '${keyName}' was updated. From ${dataFormatter(
+        current.removedData,
+      )} to ${dataFormatter(current.addedData)}`;
+    case 'parent':
+      return _.compact(
+        current.children.map(child => getChangelog(child, [...nameAcc, current.key])),
+      );
+    default:
+      return null;
+  }
 };
 
 export default diffEntries => diffEntries.flatMap(node => getChangelog(node) || []).join('\n');
