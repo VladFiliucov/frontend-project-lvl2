85d77abfbad0799373f9ba2184337a2ba617f7e9
Vlad Filiucov <filiucov@mail.ru>
2020-12-22 20:55:19 +0000

    ditto

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,11 @@
+import _ from 'lodash';
 import stylish from './stylish.js';
 import plain from './plain.js';
 
 const FORMATTERS = { stylish, plain, json: (diffEntries) => JSON.stringify(diffEntries) };
 
 export default (format, rawData) => {
-  const formatter = FORMATTERS[format];
-
-  if (formatter) return formatter(rawData);
+  if (_.has(FORMATTERS, format)) return FORMATTERS[format](rawData);
 
   throw new Error(
     `Output format ${format} is not supported. Try one of ${Object.keys(FORMATTERS).join(', ')}`,
