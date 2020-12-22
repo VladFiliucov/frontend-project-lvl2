6a90d78305175796bb0a93fb79f3d25ea177fbae
Vlad Filiucov <filiucov@mail.ru>
2020-12-22 20:41:06 +0000

    add default format to binary

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,9 +10,9 @@ const program = new Command();
 program.version('0.0.1', '-v, --version', 'output the current version');
 program.description('Compares two configuration files and shows the difference.');
 program.helpOption('-h, --help', 'output usage information');
-program.option('-f, --format [type]', 'output format');
+program.option('-f, --format [type]', 'output format. Default format is stylish');
 program.arguments('<filepath1> <filepath2>').action((filepath1, filepath2) => {
-  console.log(gendiff(filepath1, filepath2, program.format));
+  console.log(gendiff(filepath1, filepath2, program.format || 'stylish'));
 });
 
 program.parse(process.argv);
